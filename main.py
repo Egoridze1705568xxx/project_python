ffa3e4c0b90643337d67fd27d6348bda3f8b2754
Egor <levikkk1705568@yandex.ru>
2023-07-09 20:32:03 +0300

    Работа с числом Фибаначи

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello world")
+from decimal import ROUND_CEILING
+import math
+
+n = int(input("Введите число для вычисления факториала: "))
+count = 1
+i = 0
+while i!=n:
+    i +=1
+    count *= i
+print(count)
